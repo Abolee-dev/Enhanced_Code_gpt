ea7e28fd138a51815cd5d3e01bb60633c30659d3
Abolee-dev <57711277+Abolee-dev@users.noreply.github.com>
2025-07-09 16:18:37 +0530

    Update Config.java

--- kafka-to-splunk-streaming-java/src/main/java/com/example/config/Config.java
+++ kafka-to-splunk-streaming-java/src/main/java/com/example/config/Config.java
@@ -6,4 +6,6 @@ public class Config {
     public static final String SPLUNK_HEC_URL = System.getenv().getOrDefault("SPLUNK_HEC_URL", "http://localhost:8088");
     public static final String SPLUNK_TOKEN = System.getenv().getOrDefault("SPLUNK_TOKEN", "");
     public static final String DEFAULT_SPLUNK_INDEX = System.getenv().getOrDefault("SPLUNK_INDEX", "main");
+    public static final String CUSTOM_TEXT = System.getenv().getOrDefault("CUSTOM_TEXT", "_logs");
+
 }
